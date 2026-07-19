bd1cafd3d669697761fb1d2334d5a57286a69824
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-07-19 22:29:29 +0200

    render function

--- src/js/api.js
+++ src/js/api.js
@@ -17,5 +17,22 @@ async function fetchCountries(){
 }
 
 function renderCountries(countries){
-    
+    const container = document.querySelector("#countries-container");
+    container.innerHTML = "";
+
+    countries.forEach(country => {
+        const card = document.createElement("div");
+        card.className = "country-card";
+        card.innerHTML = `
+            <img src="${country.flags.png}" alt="${country.name.common} flag" />
+                <h3>${country.name.common}</h3>
+                <p><strong>Capital:</strong> ${country.capital}</p>
+                <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
+                <p><strong>Region:</strong> ${country.region}</p>
+            
+        `;
+        container.appendChild(card)
+    })
 }
+
+fetchCountries();
