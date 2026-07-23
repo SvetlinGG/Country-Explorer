322f50f25e150f12f13417b4260654d9e4ecb368
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-07-23 11:16:39 +0200

    render countries

--- src/js/api.js
+++ src/js/api.js
@@ -1,7 +1,15 @@
-const DATA_COUNTRIES = "/public/data/countries.json";
+const API_KEY = 'rc_live_c41c0fb510224d488886aaa50fca0e14';
+const BASE_URL = "https://api.restcountries.com/countries/v5";
 
 export async function fetchAllCountries() {
-    const response = await fetch(DATA_COUNTRIES);
+    const response = await fetch(BASE_URL, {
+  headers: {
+    Authorization: `Bearer ${API_KEY}`,
+  }
+});
     if (!response.ok) throw new Error(`HTTP error: ${response.status}`);
     return response.json();
+
+    const data = await response.json();
+    console.log(data);
 }
